fff665c89143966625bea4330e3fe4713131e84e
teetbox <teetbox@gmail.com>
2018-05-28 11:13:32 +0800

    Update getOrCreate function

--- ValueTypedCoreData/Core Data Service/ManagedObjectProtocol.swift
+++ ValueTypedCoreData/Core Data Service/ManagedObjectProtocol.swift
@@ -17,36 +17,14 @@ protocol ManagedObjectProtocol {
 extension ManagedObjectProtocol where Self: NSManagedObject {
     
     static func getOrCreate(withId uuid: String, in context: NSManagedObjectContext) -> Self {
-        let result = object(withId: uuid, in: context) ?? insert(in: context)
-        result.setValue(uuid, forKey: "uuid")
-        return result
-    }
-    
-    static func object(withId uuid: String, in context: NSManagedObjectContext) -> Self? {
-        let predicate = NSPredicate(format: "uuid == %@", uuid)
-        return object(in: context, with: predicate, sortDescriptors: nil)
-    }
-    
-    static func object(in context: NSManagedObjectContext, with predicate: NSPredicate?, sortDescriptors: [NSSortDescriptor]?) -> Self? {
-        return fetch(from: context, with: predicate, sortDescriptors: sortDescriptors, fetchLimit: 1)?.first
-    }
-    
-    static func insert(in context: NSManagedObjectContext) -> Self {
-        return Self(context: context)
-    }
-    
-    static func fetch(from context: NSManagedObjectContext, with predicate: NSPredicate?, sortDescriptors: [NSSortDescriptor]?, fetchLimit: Int?) -> [Self]? {
         let fetchRequest = Self.fetchRequest()
-        fetchRequest.predicate = predicate
-        fetchRequest.sortDescriptors = sortDescriptors
+        fetchRequest.predicate = NSPredicate(format: "uuid == %@", uuid)
+        fetchRequest.sortDescriptors = nil
         fetchRequest.returnsObjectsAsFaults = false
-        
-        if let limit = fetchLimit {
-            fetchRequest.fetchLimit = limit
-        }
+        fetchRequest.fetchLimit = 1
         
         var result: [Self]?
-        context.performAndWait { () -> Void in
+        context.performAndWait {
             do {
                 result = try context.fetch(fetchRequest) as? [Self]
             } catch {
@@ -54,7 +32,16 @@ extension ManagedObjectProtocol where Self: NSManagedObject {
                 NSLog("Core Data fetch error: \(error)")
             }
         }
-        return result
+        
+        // Return the existing object
+        if let object = result?.first {
+            return object
+        }
+        
+        // Return the new created one
+        let object = Self(context: context)
+        object.setValue(uuid, forKey: "uuid")
+        return object
     }
     
 }
